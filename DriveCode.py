3baa02727bb470bc1b11084910a4a7f2e9061fe2
Collin M <57117254+Lavertas55@users.noreply.github.com>
2021-03-05 18:36:09 -0600

    Adding team A features to Team B - Noah

--- DriveCode.py
+++ DriveCode.py
@@ -8,10 +8,13 @@
 import sys
 from PCA9685 import PCA9685
 import Gamepad
-import time
+import RPi.GPIO as GPIO
 
 pwm = PCA9685(0x40)
 pwm.setPWMFreq(50)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 if Gamepad.available():
     gamepad = Gamepad.smx()
@@ -27,6 +30,10 @@ gamepad.startBackgroundUpdates()
     Define Variables
 
 ---------------------------------'''
+
+controllerEnabled = False
+reverseHead = False
+shooterEnabled = False
 
 center = 1500
 shooterRange = 1000
@@ -44,12 +51,28 @@ servo = 3
 servoLeft=4
 servoRight=5
 
+enableLED = 18
+
+GPIO.setup(enableLED, GPIO.OUT)
+
 
 '''---------------------------------
 
     Generic Methods
 
 ---------------------------------'''
+
+def remapServoPosition(oldValue):
+    oldMin = 0
+    oldMax = 180
+    oldRange = oldMax - oldMin
+
+    newMin = 500
+    newMax = 2500
+    newRange = newMax - newMin
+    
+    value = (((oldValue - oldMin) * newRange) / oldRange) + newMin
+    return value
 
 def remapShooter(rawInput, cntAdj, pwrAdj):
     #Method to remap shooter motor PWM value
@@ -65,6 +88,27 @@ def exitProc():
     print("---Controller Disconnected---")
     gamepad.disconnect()
 
+def enableHandlers():
+    gamepad.addButtonPressedHandler("A", pressA)
+    gamepad.addButtonPressedHandler("B", pressB)
+    #gamepad.addButtonPressedHandler("X", pressX)
+
+    gamepad.addButtonPressedHandler("LB", pressLB)
+    gamepad.addButtonPressedHandler("RB", pressRB)
+    gamepad.addButtonPressedHandler("LT", pressLT)
+    gamepad.addButtonPressedHandler("RT", pressRT)
+
+    gamepad.addButtonReleasedHandler("LT", releaseLT)
+    #gamepad.addButtonReleasedHandler("X", releaseX)
+    #gamepad.addButtonReleasedHandler("B", releaseB)
+
+    gamepad.addAxisMovedHandler("LEFT-Y", moveLeftY)
+    gamepad.addAxisMovedHandler("RIGHT-Y", moveRightY)
+    gamepad.addAxisMovedHandler("DPAD-Y", moveStraight)
+
+def enableGenearicHandlers():
+    gamepad.addButtonPressedHandler("START", pressSTART)
+    gamepad.addButtonPressedHandler("BACK", exitProc)
 
 
 '''---------------------------------
@@ -77,13 +121,42 @@ def exitProc():
 
 def pressA():
     #when pressed, turn shooter motor on
-    pwm.setServoPulse(shooter, remapShooter(1, centerAdj, shooterPowerAdj))
-    print("Motor: ON   Output:", remapShooter(1, centerAdj, shooterPowerAdj), "    Precentage:", shooterPowerAdj)
+    global shooterEnabled
+
+    if shooterEnabled == False:
+        pwm.setServoPulse(shooter, remapShooter(1, centerAdj, shooterPowerAdj))
+        print("Motor: ON   Output:", remapShooter(1, centerAdj, shooterPowerAdj), "    Precentage:", shooterPowerAdj)
+        shooterEnabled = True
+    else:
+        pwm.setServoPulse(shooter, remapShooter(0, centerAdj, shooterPowerAdj))
+        print("Shooter: OFF")
+        shooterEnabled = False
 
 def pressB():
-    #when pressed, turn shooter motor off
-    pwm.setServoPulse(shooter, remapShooter(0, centerAdj, shooterPowerAdj))
-    print("Motor: OFF")
+    #when pressed, print pressed
+    print("Pressed B")
+
+def pressY():
+    #When pressed, print pressed
+    print("Pressed Y")
+
+def pressSTART():
+    #When pressed, toggle disable controller
+    global controllerEnabled
+    if controllerEnabled == False:
+        controllerEnabled = True
+        print("Pressed START, Controller:    Enabled")
+        GPIO.output(enableLED, GPIO.HIGH)
+        enableHandlers()
+
+    else:
+        controllerEnabled = False
+        print("Pressed START, Controller:    Disabled")
+        GPIO.output(enableLED, GPIO.LOW)
+        gamepad.removeAllEventHandlers()
+        enableGenearicHandlers()
+
+###     Triggers
 
 def pressRT():
     #when pressed, turn servo
@@ -97,21 +170,20 @@ def releaseRT():
     pwm.setServoPulse(servoRight, 1500)
     print("Released X: Servo set to 0 deg")
 
-def pressY():
-    #When pressed, print pressed
-    print("Pressed Y")
-
-
-###    Triggers
 
 def pressLB():
     #When pressed, decrease shooter speed range
     global shooterPowerAdj
+    global shooterEnabled
     if shooterPowerAdj <= 0.3:
         print("range is minimized")
     else:
         shooterPowerAdj -= 0.05
         print("range decreased, new shooterPowerAdj = ", shooterPowerAdj)
+
+    if shooterEnabled == True:
+        pwm.setServoPulse(shooter, remapShooter(1, centerAdj, shooterPowerAdj))
+        print("Shooter speed DECREASED. New Speed: ", remapShooter(1, centerAdj, shooterPowerAdj))
 
 def pressRB():
     #When pressed, increase shooter speed range
@@ -121,6 +193,10 @@ def pressRB():
     else:
         shooterPowerAdj += 0.05
         print("range increased, new shooterPowerAdj = ", shooterPowerAdj)
+
+    if shooterEnabled == True:
+        pwm.setServoPulse(shooter, remapShooter(1, centerAdj, shooterPowerAdj))
+        print("Shooter speed INCREASED. New Speed: ", remapShooter(1, centerAdj, shooterPowerAdj))
 
 def pressLT():
     #When pressed, speed range decreased for more accurate positioning
@@ -136,9 +212,15 @@ def releaseLT():
     powerAdj = 0.5
     print("Trigger released")
 
-#def pressRT():
-    #When pressed, print pressed
-#    print("Pressed RT")
+def pressRT():
+    #When pressed, toggle head reverse
+    global reverseHead
+    if reverseHead == False:
+        reverseHead = True
+        print("Pressed RT: Head reversed = true")
+    else:
+        reverseHead = False
+        print("Pressed RT: Head reversed = false")
 
 
 
@@ -152,19 +234,38 @@ def releaseLT():
 
 def moveLeftY(position):
     #Map the left y joystick value to the left motor PWM output
-    pwm.setServoPulse(leftMotor, remapDrive(-position, centerAdj, powerAdj))
-    print("Left Motor", remapDrive(-position, centerAdj, powerAdj))
-    print("Joystick Position: ", -position)
+    global reverseHead
+
+    if reverseHead == True:
+        pwm.setServoPulse(rightMotor, remapDrive(-position, centerAdj, powerAdj))
+        print("Left Motor", remapDrive(-position, centerAdj, powerAdj))
+        print("Joystick Position: ", -position)
+    else:
+        pwm.setServoPulse(leftMotor, remapDrive(-position, centerAdj, powerAdj))
+        print("Left Motor", remapDrive(-position, centerAdj, powerAdj))
+        print("Joystick Position: ", -position)
 
 def moveRightY(position):
     #Map the right y joystick value to the right motor PWM output
-    pwm.setServoPulse(rightMotor, remapDrive(position, centerAdj, powerAdj))
-    print("Right Motor", remapDrive(position, centerAdj, powerAdj))
-    print("Joystick Position: ", position)
+    global reverseHead
+    if reverseHead == True:
+        pwm.setServoPulse(leftMotor, remapDrive(position, centerAdj, powerAdj))
+        print("Right Motor", remapDrive(position, centerAdj, powerAdj))
+        print("Joystick Position: ", position)
+    else:
+        pwm.setServoPulse(rightMotor, remapDrive(position, centerAdj, powerAdj))
+        print("Right Motor", remapDrive(position, centerAdj, powerAdj))
+        print("Joystick Position: ", position)
 
-
-
-
+def moveStraight(position):
+    global reverseHead
+    if reverseHead == True:
+        pwm.setServoPulse(rightMotor, remapDrive(-position, centerAdj, powerAdj))
+        pwm.setServoPulse(leftMotor, remapDrive(position, centerAdj, powerAdj))
+    else:
+        pwm.setServoPulse(rightMotor, remapDrive(position, centerAdj, powerAdj))
+        pwm.setServoPulse(leftMotor, remapDrive(-position, centerAdj, powerAdj))
+        print("Moving Straight")
 
 '''---------------------------------
 
@@ -172,20 +273,5 @@ def moveRightY(position):
 
 ---------------------------------'''
 
-gamepad.addButtonPressedHandler("A", pressA)
-gamepad.addButtonPressedHandler("B", pressB)
-gamepad.addButtonPressedHandler("RT", pressRT)
-gamepad.addButtonPressedHandler("Y", pressY)
-
-gamepad.addButtonPressedHandler("LB", pressLB)
-gamepad.addButtonPressedHandler("RB", pressRB)
-gamepad.addButtonPressedHandler("LT", pressLT)
-#gamepad.addButtonPressedHandler("RT", pressRT)
-
-gamepad.addButtonReleasedHandler("LT", releaseLT)
-gamepad.addButtonReleasedHandler("RT", releaseRT)
-
-gamepad.addButtonPressedHandler("BACK", exitProc)
-
-gamepad.addAxisMovedHandler("LEFT-Y", moveLeftY)
-gamepad.addAxisMovedHandler("RIGHT-Y", moveRightY)
+gamepad.removeAllEventHandlers()
+enableGenearicHandlers()
